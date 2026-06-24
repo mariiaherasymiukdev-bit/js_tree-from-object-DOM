64d6bd628e2fc708fa29ede76964c082e601cf58
Mariia Herasymiuk <mariia.herasymiuk.dev@gmail.com>
2026-06-24 13:48:01 +0200

    make build

---
+++ js_tree-from-object-DOM.75bf0a27.js
@@ -0,0 +1,2 @@
+"use strict";var food={Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}},tree=document.querySelector("#tree");function createTree(e,r){var t=document.createElement("ul");for(var n in r){var a=document.createElement("li");a.textContent=n,Object.keys(r[n]).length>0&&createTree(a,r[n]),t.append(a)}e.append(t)}createTree(tree,food);
+//# sourceMappingURL=js_tree-from-object-DOM.75bf0a27.js.map
